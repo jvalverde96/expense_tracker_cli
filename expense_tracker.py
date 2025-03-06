59ab4de7490ef4a1cf7749869b8868a7fd3192ea
jvalverde96 <javier96.vs@gmail.com>
2025-03-06 16:51:25 -0600

    Create function to initialize CSV file

--- expense_tracker.py
+++ expense_tracker.py
@@ -9,3 +9,20 @@ def generate_file_name():
     """Generate the CSV file name based on the current month and year."""
     today = datetime.today()
     return f"expenses_{today.year}_{today.month:02d}.csv"
+
+# Creates the CSV file if it doesn't exist and adds headers
+def initialize_file():
+    """Create the CSV file if it doesn't exist and add headers."""
+    expense_file = generate_file_name()
+    if not os.path.exists(expense_file):
+        with open(expense_file, mode="a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Date", "Category", "Amount (Colones)", "Amount (Dollars)", "Description"])
+
+def main():
+    initialize_file()
+    print("Main")
+
+# Run the main function if this script is executed directly
+if __name__ == "__main__":
+    main()
